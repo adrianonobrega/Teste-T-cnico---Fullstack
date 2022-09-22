d3acc5d8d05a9c3559e020c2a28d78c259f920b5
adrianonobrega <adrianonobrega26@gmail.com>
2022-09-22 19:09:29 -0300

    ds

--- dist/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-var express_1 = require("express");
-var user_1 = require("./user");
-var contact_1 = require("./contact");
-exports.router = (0, express_1.Router)();
-exports.router.use("/users", user_1.userRoutes);
-exports.router.use("/contacts", contact_1.contactRoutes);
