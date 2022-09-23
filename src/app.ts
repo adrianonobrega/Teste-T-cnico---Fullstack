737138cfb326bae434d813c6998e23b2ee68565b
adrianonobrega <adrianonobrega26@gmail.com>
2022-09-23 09:35:31 -0300

    Fix: bug cors

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import { AppDataSource } from './database'
 import { router } from './routes'
 
 const app = express()
+const cors = require("cors")
 
+app.use(cors())
 app.use(express.json())
 
 app.use(router)
